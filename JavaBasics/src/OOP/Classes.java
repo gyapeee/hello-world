514f7e79994c40bf49cb8353aaa4975863a6bf62
sinemetu <sinemetu@sinemetu-HP-EliteBook-2560p>
2018-11-03 12:20:39 +0100

    Added lux constants and current lux variable

--- JavaBasics/src/OOP/Classes.java
+++ JavaBasics/src/OOP/Classes.java
@@ -58,9 +58,19 @@ abstract class Lamp{
 	}
 }
 
-//class FlashLight extends Lamp{
-//	
-//}
+class FlashLight extends Lamp{
+
+	private double currentLux;
+	static final double LUX_OF_MAX_VOLTAGE = 12.35;
+	static final double LUX_OF_MID_VOLTAGE = 7.135;
+	static final double LUX_OF_MIN_VOLTAGE = 5.45;
+	static final double LUX_OF_LOW_VOLTAGE = 3.01;
+	static final double LUX_OF_EMPTY = 0.0;
+	
+	public double getCurrentLux() {
+		return currentLux;
+	}
+}
 
 class TableLamp extends Lamp{
 	/** State of the plug */
